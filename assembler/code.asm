c0a10659df60f6f16208c25ce1e668d76ab51747
trevisharp <38115923+trevisharp@users.noreply.github.com>
2021-02-25 19:49:45 -0300

    Correções

--- assembler/code.asm
+++ assembler/code.asm
@@ -1,8 +1,5 @@
-loop:
+minhalabel:
     mov 5 $1
     mov 7 $0
-    add $0 $1
-    nop
-    nop
-    jmp loop
-    loa $5 $6
+    cmp $0 $1
+    jg minhalabel
--- code.asm
+++ code.asm
@@ -1,9 +1,5 @@
+minhalabel:
     mov 5 $1
     mov 7 $0
-    add $0 $1
-    shl $0 $1
-    nop
-    mov 2 $2
-    sub $1 $2
-    shr $0 $1
-    nop
+    cmp $0 $1
+    jg minhalabel
