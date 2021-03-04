f7c2795bcc631d446b8b68508c67a44686d4c51a
trevisharp <38115923+trevisharp@users.noreply.github.com>
2021-03-04 19:12:09 -0300

    Screen test

--- code.asm
+++ code.asm
@@ -1,16 +1,21 @@
-    mov 0 $1
-    mov 1 $2
-    mov 255 $3
-    mov 255 $4
-    add $3 $4
-    add $3 $4
-    add $3 $4
-    add $3 $4
-    mov 71 $4
-    add $3 $4
-loop:
-    add $1 $2
-    cmp $1 $3
-    jne loop
+    mov 10 $0
+    mov 60 $31
+    mul $31 $0
+    mul $31 $0
+    mul $31 $0
+    mov 128 $0
+    str $0 $31
+    mov 1 $1
+    add $31 $1
+    str $0 $31
+    add $31 $1
+    add $31 $1
+    str $1 $31
+    sub $31 $1
+    mov 31 $0
+    str $0 $31
+    mov 0 $0
+    add $31 $1
+    str $0 $31
 end:
     jmp end
