f751354a04af393280f35daa227f795f509af580
trevisharp <38115923+trevisharp@users.noreply.github.com>
2021-03-04 18:32:25 -0300

    Load, Store, Screen implementation

--- code.asm
+++ code.asm
@@ -1,5 +1,16 @@
-minhalabel:
-    mov 5 $1
-    mov 7 $0
-    cmp $0 $1
-    jg minhalabel
+    mov 0 $1
+    mov 1 $2
+    mov 255 $3
+    mov 255 $4
+    add $3 $4
+    add $3 $4
+    add $3 $4
+    add $3 $4
+    mov 71 $4
+    add $3 $4
+loop:
+    add $1 $2
+    cmp $1 $3
+    jne loop
+end:
+    jmp end
