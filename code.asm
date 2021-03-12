55bb8b52c9e00883903b77bcf4af098580675ebc
Lolkih <78231384+Lolkih@users.noreply.github.com>
2021-03-12 17:45:50 -0300

    código mto loco

--- code.asm
+++ code.asm
@@ -1,3 +1,6 @@
+    mov 0 $3
+    mov 0 $4
+    mov 32 $5
     mov 10 $0
     mov 60 $31
     mul $31 $0
@@ -5,17 +8,25 @@
     mul $31 $0
     mov 128 $0
     str $0 $31
+    mov 3 $1
+    add $31 $1
+    mov $30 $31
+    mov $29 $31
+    mov $28 $31
     mov 1 $1
-    add $31 $1
-    str $0 $31
-    add $31 $1
-    add $31 $1
+    sub $30 $1
+    mov 2 $1
+    sub $29 $1
+    mov 3 $1
+    sub $28 $1
+    mov 1 $1
+    str $1 $28
+desenha:
     str $1 $31
-    sub $31 $1
-    mov 31 $0
-    str $0 $31
-    mov 0 $0
-    add $31 $1
-    str $0 $31
+    str $3 $31
+    add $4 $1
+    str $4 $30
+    cmp $5 $4
+    jg desenha
 end:
-    jmp end
+    jmp end    
