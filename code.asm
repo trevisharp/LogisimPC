12bd8fcd93dd36aa63a811df28f2f5d2e502d8e0
trevisharp <38115923+trevisharp@users.noreply.github.com>
2021-03-25 19:05:08 -0300

    Implementação do drawhorizontalline

--- code.asm
+++ code.asm
@@ -5,3 +5,42 @@
     mov 129 $20
     mov 255 $21
     call drawinallcolumns
+
+border:
+    mov 163 $1
+    mov 255 $2 //0000000011111111 
+    mov 8 $3
+    mov 255 $4
+    mov 100 $0
+    mov 101 $5
+    mov 162 $6  
+    mov 1 $7
+    mov 102 $9
+    mov 161 $10
+    mov 2 $11
+    mov 162 $12
+    mov 101 $13
+    mov 128 $14 //0000000010000000
+    
+    shl $14 $3
+    shl $2 $3
+    add $2 $4
+    str $2 $1
+    str $2 $0
+    str $2 $5
+    str $2 $6
+    
+loopparborder:
+    str $7 $9
+    add $9 $11
+    cmp $9 $12
+    jne loopparborder
+
+loopimparborder:
+    str $14 $10
+    sub $10 $11
+    cmp $10 $13
+    jne loopimparborder
+    
+    call draw
+    ret
--- pong/pong.asm
+++ pong/pong.asm
@@ -1,45 +1,118 @@
-    mov 163 $1
-    mov 255 $2 //0000000011111111 
-    mov 8 $3
-    mov 255 $4
+    mov 0 $6
+    mov 1 $5
+main:
     mov 100 $0
-    mov 101 $5
-    mov 162 $6  
-    mov 1 $7
-    mov 102 $9
-    mov 161 $10
-    mov 2 $11
-    mov 162 $12
-    mov 101 $13
-    mov 128 $14 //0000000010000000
-    
-    shl $14 $3
-    shl $2 $3
-    add $2 $4
-    str $2 $1
-    str $2 $0
-    str $2 $5
-    str $2 $6
-    
-looppar:
-    str $7 $9
-    add $9 $11
-    cmp $9 $12
-    jne looppar
+    call clear
 
-loopimpar:
-    str $14 $10
-    sub $10 $11
-    cmp $10 $13
-    jne loopimpar
+    mov 100 $0
+    mov 20 $1
+    mov $2 $6
+    mov 10 $3
+    mov 100 $0
+    call drawhorizontalline
 
-     
+    mov 100 $0
     call draw
 
-    
+    add $6 $5
+    jmp main
 
 end:
     jmp end
+
+drawhorizontalline:
+    mov 15 $25
+    mov 1 $26
+    cmp $2 $25
+    jg drawhorizontalline_baixo
+
+    sub $25 $2
+    shl $26 $25
+
+    mov 1 $27
+    add $27 $0
+    mov 2 $28
+    mul $1 $28 // x = 2 * x
+    add $27 $1 // $27 = $27 + 2 * x
+    mov 0 $29 // len = 0
+    mul $3 $28 // comprimento = comprimento * 2
+    add $29 $3 // len = 2 * comprimento
+    add $29 $27 // len = $27 + 2 * comprimento
+
+drawhorizontalline_loop1:
+    loa $30 $27
+    or $30 $26
+    str $30 $27
+    add $27 $28 // i += 2
+    cmp $27 $29
+    jne drawhorizontalline_loop1 //i != len
+    jmp drawhorizontalline_end
+
+drawhorizontalline_baixo:
+    mov 31 $25
+    sub $25 $2
+    shl $26 $25
+
+    mov 0 $27
+    add $27 $0
+    mov 2 $28
+    mul $1 $28 // x = 2 * x
+    add $27 $1 // $27 = $27 + 2 * x
+    mov 0 $29 // len = 0
+    mul $3 $28 // comprimento = comprimento * 2
+    add $29 $3 // len = 2 * comprimento
+    add $29 $27 // len = $27 + 2 * comprimento
+
+drawhorizontalline_loop2:
+    loa $30 $27
+    or $30 $26
+    str $30 $27
+    add $27 $28 // i += 2
+    cmp $27 $29
+    jne drawhorizontalline_loop2 //i != len
+
+    jmp drawhorizontalline_end
+
+drawhorizontalline_end:
+    ret
+
+drawverticalline:
+    ret
+
+drawrect:
+    mov $16 $0
+    mov $17 $1
+    mov $18 $2
+    mov $19 $3
+    mov $20 $4
+
+    mov $0 $16
+    mov $1 $17
+    mov $2 $18
+    mov $3 $19
+    call drawhorizontalline
+
+    mov $0 $16
+    mov $1 $17
+    mov $2 $18
+    add $2 $4
+    mov $3 $19
+    call drawhorizontalline
+
+    ret
+
+clear:
+    mov 0 $1
+    mov 0 $2
+    mov 1 $3
+    mov 64 $4
+    add $4 $0
+clearloop:
+    str $1 $0
+    add $0 $3
+    cmp $4 $0
+    jne clearloop
+    ret
 
 draw:
     mov 0 $28
