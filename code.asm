626d9b69fb479e05a041d2def3357e97d6adede7
Lolkih <78231384+Lolkih@users.noreply.github.com>
2021-03-25 17:32:49 -0300

    pong.asm borda

--- code.asm
+++ code.asm
@@ -5,39 +5,3 @@
     mov 129 $20
     mov 255 $21
     call drawinallcolumns
-end:
-    jmp end
-
-drawinallcolumns:
-    mov 0 $3
-    mov 0 $4
-    mov 5 $5
-    mov 1 $0
-    mov 60 $31
-    mul $31 $0
-    mul $31 $0
-    mul $31 $0
-    mov 128 $0
-    str $0 $31
-    mov 3 $1
-    add $31 $1
-    mov $30 $31
-    mov $29 $31
-    mov $28 $31
-    mov 1 $1
-    sub $30 $1
-    mov 2 $1
-    sub $29 $1
-    mov 3 $1
-    sub $28 $1
-    mov 1 $1
-    str $16 $28
-    str $17 $29
-    str $1 $31
-desenha:
-    add $4 $1
-    str $4 $30
-    cmp $5 $4
-    jg desenha
-    str $3 $31
-    ret
--- pong/pong.asm
+++ pong/pong.asm
@@ -1,10 +1,42 @@
-    mov 161 $1
-    mov 1 $2
-    mov 15 $3
-    shl $2 $3
-    str $2 $1
+    mov 163 $1
+    mov 255 $2 //0000000011111111 
+    mov 8 $3
+    mov 255 $4
     mov 100 $0
+    mov 101 $5
+    mov 162 $6  
+    mov 1 $7
+    mov 102 $9
+    mov 161 $10
+    mov 2 $11
+    mov 162 $12
+    mov 101 $13
+    mov 128 $14 //0000000010000000
+    
+    shl $14 $3
+    shl $2 $3
+    add $2 $4
+    str $2 $1
+    str $2 $0
+    str $2 $5
+    str $2 $6
+    
+looppar:
+    str $7 $9
+    add $9 $11
+    cmp $9 $12
+    jne looppar
+
+loopimpar:
+    str $14 $10
+    sub $10 $11
+    cmp $10 $13
+    jne loopimpar
+
+     
     call draw
+
+    
 
 end:
     jmp end
